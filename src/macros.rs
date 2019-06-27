a3fcf9c00577b6aeb839001d7a17b22c98b69f55
Roman Gafiyatullin <r.gafiyatullin@me.com>
2019-06-27 13:36:07 +0300

    0.2.2: include_json_as! macro added

--- src/macros.rs
+++ src/macros.rs
@@ -1,4 +1,4 @@
-#[macro_export]
+// #[macro_export]
 macro_rules! enum_variant_from {
     ($target: ident, $variant: ident, $source: ident) => {
         impl From<$source> for $target {
@@ -9,7 +9,7 @@ macro_rules! enum_variant_from {
     };
 }
 
-#[macro_export]
+// #[macro_export]
 macro_rules! enum_variant_into_result {
     ($source: ident, $variant: ident, $target: ident) => {
         impl Into<Result<$target, $source>> for $source {
@@ -20,5 +20,12 @@ macro_rules! enum_variant_into_result {
                 }
             }
         }
+    };
+}
+
+#[macro_export]
+macro_rules! include_json_as {
+    ($deserialize_as: ident, $file: expr) => {
+        serde_json::from_str::<$deserialize_as>(include_str!($file)).unwrap()
     };
 }
