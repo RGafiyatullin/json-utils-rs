e2b460dc61bab863007813ccecac5b9977eaae2e
Roman Gafiyatullin <r.gafiyatullin@me.com>
2019-10-13 21:32:01 +0300

    0.3.4: export JsMap,JsNumber,JsError

--- src/json/js_value.rs
+++ src/json/js_value.rs
@@ -1,1 +1,5 @@
 pub use serde_json::Value as JsValue;
+pub use serde_json::Number as JsNumber;
+pub use serde_json::Map as JsMap;
+
+pub use serde_json::Error as JsError;
--- src/json/mod.rs
+++ src/json/mod.rs
@@ -2,6 +2,10 @@ mod js_value;
 mod query_for_js_value;
 
 pub use js_value::JsValue;
+pub use js_value::JsNumber;
+pub use js_value::JsMap;
+
+pub use js_value::JsError;
 
 #[cfg(test)]
 mod query_tests;
